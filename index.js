7af6d86717a5b86a26d16a2744480202b5621429
Ubuntu <ubuntu@ip-172-31-46-170.ap-south-1.compute.internal>
2026-04-16 07:19:41 +0000

    add github actions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello from EC2 deployment!");
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from EC2 deployment!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
